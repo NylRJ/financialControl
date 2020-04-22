3010acac877cdd177acf17f1bc9d661304719f77
Moises <loveloungeoficial@gmail.com>
2020-04-22 04:10:46 -0300

    Configuracao do security JWT co permicoes de GET e negacoes de outros metodos

--- src/main/java/com/i9developed/depmoneyapi/config/SecurityConfig.java
+++ src/main/java/com/i9developed/depmoneyapi/config/SecurityConfig.java
@@ -1,30 +1,63 @@
 package com.i9developed.depmoneyapi.config;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
-
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("admin").password("admin").roles("ROLE");
-	}
+	@Autowired
+	private Environment env;
 	
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-			.antMatchers("/categorias").permitAll()
-			.anyRequest().authenticated()
-			.and().httpBasic()
-			.and().sessionManagement()
-			.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().csrf().disable();
+	private static final String[] PUBLIC_MATCHERS = {
+		"/localhost/depmaneyapi/**",
+		"/categorias/**"
+			
+	};
+	
+	private static final String[] PUBLIC_MATCHERS_GET = {
+			
+			"/lancamento/**",
+			"/pessoas/**"
+				
+		};
 		
+	
+	 @Override
+	protected void configure(HttpSecurity http) throws Exception {
+		 if(Arrays.asList(env.getActiveProfiles()).contains("test")) {
+			 
+			 http.headers().frameOptions().disable();
+		 }
+		 
+		 http.cors().and().csrf().disable();
+		 http.authorizeRequests()
+		 .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+		 .antMatchers(PUBLIC_MATCHERS).permitAll()
+		 .anyRequest().authenticated();
+		 http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
+	 
+	  @Bean
+	  CorsConfigurationSource corsConfigurationSource() {
+	    final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+	    source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+	    return source;
+	  }
+	 
+	 
+	 
 }
